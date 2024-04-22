54eeb94b7e5644aacca9ef291e1efb10422084ad
rchoorikkadu <rchoorikkadu@nucleara.com>
2024-04-22 18:15:19 +0100

    single stage - to single command adjusted

--- scripts/run-sedge.sh
+++ scripts/run-sedge.sh
@@ -3,24 +3,25 @@
 echo "Home directory: $HOME"
 cd src/build
 echo 'Running sedge...'
-#./sedge generate --logging none \
-#  -p $HOME full-node \
-#  --map-all \
-#  --no-mev-boost \
-#  --no-validator \
-#  --network chiado \
-#  -c lighthouse:sigp/lighthouse:latest \
-#  -e nethermind:nethermindeth/nethermind:master \
-#  --el-extra-flag Sync.NonValidatorNode=true \
-#  --el-extra-flag Sync.DownloadBodiesInFastSync=false \
-#  --el-extra-flag Sync.DownloadReceiptsInFastSync=false \
-#  --cl-extra-flag checkpoint-sync-url=http://139.144.26.89:4000/ \
-#  >> $HOME/sedge.logs
-./sedge generate --logging none -p $HOME full-node --map-all --no-mev-boost --no-validator --network chiado -c lighthouse:sigp/lighthouse:latest -e nethermind:nethermindeth/nethermind:master --el-extra-flag Sync.NonValidatorNode=true --el-extra-flag Sync.DownloadBodiesInFastSync=false --el-extra-flag Sync.DownloadReceiptsInFastSync=false --cl-extra-flag checkpoint-sync-url=http://139.144.26.89:4000/ >> $HOME/sedge.logs
+./sedge generate --logging none \
+  -p $HOME full-node \
+  --map-all \
+  --no-mev-boost \
+  --no-validator \
+  --network chiado \
+  -c lighthouse:sigp/lighthouse:latest \
+  -e nethermind:nethermindeth/nethermind:master \
+  --el-extra-flag Sync.NonValidatorNode=true \
+  --el-extra-flag Sync.DownloadBodiesInFastSync=false \
+  --el-extra-flag Sync.DownloadReceiptsInFastSync=false \
+  --cl-extra-flag checkpoint-sync-url=http://139.144.26.89:4000/ \
+  >> $HOME/sedge.logs
 
 echo "Current directory:${pwd}"
 cp $HOME/docker-compose.yml .
+cp $HOME/.env .
+cat docker-compose.yml
 echo "Copied docker-compose file"
-ls -ltr
+ls -lta
 ./sedge run -p . >> $HOME/sedge.logs
 curl http://localhost:8545/
